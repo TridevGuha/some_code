0742bd69150b3a73cd89aae159374cee43bd7727
Tridev <tridevguha17@gmail.com>
2021-07-08 20:38:28 +0530

    update testscript.sh
    one line removed and one new line added

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
 echo "this was run automatically"
-echo "this is a test script"
 echo "this is a new line to check git scm pulling with jenkins"
+echo "this is complete"
